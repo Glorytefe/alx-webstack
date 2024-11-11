a36aaf6ddb001e3965abdab1d2780b5a51874f32
glorytefe <glorytefe@gmail.com>
2024-11-11 22:22:55 +0100

    update post

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -8,7 +8,7 @@ router.get('/', postController.getPosts);
 router.get('/search', postController.searchPosts);
 router.post('/', requireAuthAsync, postController.createPost);
 router.get('/:id', requireAuthAsync, postController.getPostById);
-router.patch('/:id', requireAuthAsync, postController.updatePost);
+router.put('/:id', requireAuthAsync, postController.updatePost);
 router.delete('/:id', requireAuthAsync, postController.deletePost);
 router.post('/:id/comments', requireAuthAsync, postController.addCommentToPost);
 router.delete('/:postId/comments/:commentId', requireAdmin, postController.deleteCommentFromPost);
